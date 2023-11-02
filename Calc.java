e5e1a76415a4df44b0f73e820525483a05537256
Anatoliy6463 <120381885+Anatoliy6463@users.noreply.github.com>
2023-11-02 23:40:45 +0300

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -7,7 +7,7 @@ class Calc {
         double n1 = k.nextInt();
         double n2 = k.nextInt();
         System.out.println ("Что вы хотите сделать с этими числами? + = 1, - = 2, x = 3, / = 4, ^ = 5");
-        char  deistviye = i.nextInt();
+        int  deistviye = i.nextInt();
         double otvet = 0;
         if (deistviye == 1){
             otvet = n1 + n2;
