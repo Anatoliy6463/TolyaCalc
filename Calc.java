013a1d5805a3149b479e010027700da2a797f244
Anatoliy6463 <120381885+Anatoliy6463@users.noreply.github.com>
2023-11-02 22:34:23 +0300

    Create Calc.java

---
+++ Calc.java
@@ -0,0 +1,33 @@
+import java.util.Scanner; 
+ 
+class Calc {
+    public static void main(String args[]) {
+        System.out.println ("Введите два целых числа");
+		Scanner k = new Scanner(System.in);
+        int n1 = k.nextInt();
+        int n2 = k.nextInt();
+        System.out.println ("Что вы хотите сделать с этими числами? + = 1, - = 2, x = 3, / = 4");
+        Scanner l = new Scanner(System.in);
+        int  deistviye = l.nextInt();
+        int otvet = 0;
+        if (deistviye == 1){
+            otvet = n1 + n2;
+            System.out.println("Ответ: " + otvet);
+        } 
+        else if (deistviye == 2){
+            otvet = n1 - n2;
+            System.out.println("Ответ: " + otvet);
+        } 
+        else if (deistviye == 3){
+            otvet = n1 * n2;
+            System.out.println("Ответ: " + otvet);
+        } 
+        else if (deistviye == 4 && n2 != 0){
+            otvet = n1 / n2;
+            System.out.println("Ответ: " + otvet);
+        }
+        else if (deistviye == 4 && n2 == 0){
+            System.out.println("НА НОЛЬ ДЕЛИТЬ НЕЛЬЗЯ");
+        }
+	}
+}
