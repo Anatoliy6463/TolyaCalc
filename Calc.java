a48fb939b94d425ee36d436bf6b772046d502c92
Anatoliy6463 <120381885+Anatoliy6463@users.noreply.github.com>
2023-11-02 22:46:18 +0300

    Update Calc.java
    Added exponentiation

--- Calc.java
+++ Calc.java
@@ -1,4 +1,3 @@
-/* My simple calculator */
 import java.util.Scanner; 
  
 class Calc {
@@ -7,28 +6,37 @@ class Calc {
 		Scanner k = new Scanner(System.in);
         int n1 = k.nextInt();
         int n2 = k.nextInt();
-        System.out.println ("Что вы хотите сделать с этими числами? + = 1, - = 2, x = 3, / = 4");
+        System.out.println ("Что вы хотите сделать с этими числами? + = 1, - = 2, x = 3, / = 4, ^ = 5");
         Scanner l = new Scanner(System.in);
         int  deistviye = l.nextInt();
         int otvet = 0;
         if (deistviye == 1){
             otvet = n1 + n2;
-            System.out.println("Ответ: " + otvet);
+            System.out.println(n1 + " + " + n2 + "=" + otvet);
         } 
         else if (deistviye == 2){
             otvet = n1 - n2;
-            System.out.println("Ответ: " + otvet);
+            System.out.println(n1 + " - " + n2 + "=" + otvet);
         } 
         else if (deistviye == 3){
             otvet = n1 * n2;
-            System.out.println("Ответ: " + otvet);
+            System.out.println(n1 + " x " + n2 + "=" + otvet);
         } 
         else if (deistviye == 4 && n2 != 0){
             otvet = n1 / n2;
-            System.out.println("Ответ: " + otvet);
+            System.out.println(n1 + " / " + n2 + "=" + otvet);
         }
         else if (deistviye == 4 && n2 == 0){
             System.out.println("НА НОЛЬ ДЕЛИТЬ НЕЛЬЗЯ");
+        }
+        else if (deistviye == 5 && n2 != 0){
+            int n4 = 1;
+            int n3 = n1;
+            while(n4 <= n2) {
+                n4++;
+                n3 = n1*n2;
+            }
+            System.out.println (n1 + " ^ " + n2 + " = " + n3;);
         }
 	}
 }
