8164207e08264f51032e3ecd64f7f151a1189c1a
Anatoliy6463 <120381885+Anatoliy6463@users.noreply.github.com>
2023-11-03 16:05:14 +0300

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -1,43 +1,42 @@
 import java.util.Scanner; 
- 
 class Calc {
     public static void main(String args[]) {
+        Scanner k = new Scanner(System.in);
         System.out.println ("Введите два числа");
-		Scanner k = new Scanner(System.in);
         double n1 = k.nextInt();
         double n2 = k.nextInt();
-        System.out.println ("Что вы хотите сделать с этими числами? + = 1, - = 2, x = 3, / = 4, ^ = 5");
-        int  deistviye = i.nextInt();
+        System.out.println ("Что вы хотите сделать с этими числами? ");
+        char deistviye = k.next().charAt(0);
         double otvet = 0;
-        if (deistviye == 1){
+        if (deistviye == '+'){
             otvet = n1 + n2;
             System.out.println(n1 + " + " + n2 + "=" + otvet);
         } 
-        else if (deistviye == 2){
+        else if (deistviye == '-'){
             otvet = n1 - n2;
             System.out.println(n1 + " - " + n2 + "=" + otvet);
         } 
-        else if (deistviye == 3){
+        else if (deistviye == '*'){
             otvet = n1 * n2;
             System.out.println(n1 + " x " + n2 + "=" + otvet);
         } 
-        else if (deistviye == 4 && n2 != 0){
+        else if (deistviye == '/' && n2 != 0){
             otvet = n1 / n2;
             System.out.println(n1 + " / " + n2 + "=" + otvet);
         }
-        else if (deistviye == 4 && n2 == 0){
+        else if (deistviye == '/' && n2 == 0){
             System.out.println("НА НОЛЬ ДЕЛИТЬ НЕЛЬЗЯ");
         }
-        else if (deistviye == 5 && n2 != 0){
+        else if (deistviye == '^' && n2 != 0){
             double a = n1;
-            long int n = n2;
+            double n = n2;
             for (int o = 1; o < n; o++) {
                 a *= n2;
             }
             System.out.println (n1 + " ^ " + n2 + " = " + a);
         }
-         else if (deistviye == 5 && n2 == 0){
-            System.out.println (n1 + " ^ 0  = 0");
+         else if (deistviye == '^' && n2 == 0){
+            System.out.println (n1 + " ^ 0  = 1");
         }
 	}
 }
